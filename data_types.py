0b5f5dabb4ca6abfb43f24845a1f5f214737df19
Monika <mstopins@student.agh.edu.pl>
2022-04-11 15:12:34 +0200

    System function test

--- data_types.py
+++ data_types.py
@@ -51,4 +51,22 @@ full_sorted = sorted(full, reverse=True)
 # Print out full_sorted
 print(full_sorted)
 
+# Create a list containing the names: baby_names
+baby_names = ['Ximena', 'Aliza', 'Ayden', 'Calvin']
+
+# Extend baby_names with 'Rowen' and 'Sandeep'
+baby_names.extend(['Rowen', 'Sandeep'])
+
+# Print baby_names
+print(baby_names)
+
+# Find the position of 'Aliza': position
+position = baby_names.index('Aliza')
+
+# Remove 'Aliza' from baby_names
+baby_names.pop(position)
+
+# Print baby_names
+print(baby_names)
+
 
